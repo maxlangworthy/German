e3f42074e1173051dd48238999f7947cae36f556
maxlangworthy <max.langworthy@tonygee.com>
2026-08-11 15:35:17 +0100

    Create romanized-openings.js
    Romanized openings added

---
+++ romanized-openings.js
@@ -0,0 +1,37 @@
+export const ROMANIZED_OPENINGS = {
+  coffee: {
+    mandarin_pinyin: {
+      reply: "Nǐ hǎo! Nǐ xiǎng diǎn shénme?",
+      replyWords: [],
+    },
+
+    japanese_romaji: {
+      reply: "Konnichiwa! Nani o chūmon shimasu ka?",
+      replyWords: [],
+    },
+  },
+
+  weekend: {
+    mandarin_pinyin: {
+      reply: "Nǐ hǎo! Nǐ zhōumò guò de zěnmeyàng?",
+      replyWords: [],
+    },
+
+    japanese_romaji: {
+      reply: "Konnichiwa! Shūmatsu wa dō deshita ka?",
+      replyWords: [],
+    },
+  },
+
+  directions: {
+    mandarin_pinyin: {
+      reply: "Bù hǎoyìsi, qǐngwèn huǒchēzhàn zěnme zǒu?",
+      replyWords: [],
+    },
+
+    japanese_romaji: {
+      reply: "Sumimasen, eki made dō yatte ikeba ii desu ka?",
+      replyWords: [],
+    },
+  },
+};
